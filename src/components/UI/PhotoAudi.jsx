5525b61d5cd8cc7607c20439e35320bf8ef78fcd
andriydrob <drobandriy2000@gmail.com>
2025-11-23 17:13:12 +0200

    video element completely changed

--- src/components/UI/PhotoAudi.jsx
+++ src/components/UI/PhotoAudi.jsx
@@ -1,49 +1,75 @@
 import Btn from './Btn';
 
-
-const PhotoAudi = ({photo, title, subtitle}) => {
-    return(
-        <div className='relative w-full bg-[#191919] flex items-center justify-center overflow-hidden'>
-            
-            <div className='max-w-full h-auto max-h-screen'>
-                <img src={photo} alt="Car Photo" className="w-full h-auto block" />
+const PhotoAudi = ({ photo, title, subtitle }) => {
+    return (
+        <div 
+            className='
+                relative 
+                h-screen
+                w-full 
+                bg-[#191919] 
+                overflow-hidden
+                flex items-center justify-center
+            '
+        >
+            <div className='absolute inset-0'>
+                <img 
+                    src={photo} 
+                    alt="Car Photo" 
+                    className="
+                        w-full h-full
+                        object-cover
+                        block
+                    " 
+                />
             </div>
-
-            <div className="absolute inset-0 bg-black/50 opacity-40 z-[5px]"></div>
             
-            <div className="absolute bottom-4 left-5 md:bottom-10 md:left-10 z-10">
+            <div 
+                className="
+                    absolute inset-0 
+                    bg-gradient-to-t 
+                    from-black/70 via-black/30 to-black/20 
+                    z-10 
+                "
+            ></div>
+            
+            <div className="
+                absolute 
+                bottom-10 left-5 md:left-10 md:bottom-20
+                z-20
+                w-[90%] md:w-auto
+            ">
                 
                 <div className="flex flex-col mb-4 md:mb-6">
-                    <h1 className="Futura New font-bold text-[14px] md:text-6xl text-white">{title}</h1>
-                    <p className="Futura New text-[10px] md:text-2xl text-white inline-block">{subtitle}</p>
+                    <h1 className="Futura New font-bold text-4xl sm:text-6xl md:text-7xl text-white leading-none mb-2">{title}</h1>
+                    <p className="Futura New text-lg sm:text-2xl md:text-3xl text-white inline-block">{subtitle}</p>
                 </div>
 
                 <div className='flex space-x-3 md:space-x-4'>
                     <Btn className="
-                        bg-[#B5B7B8] text-black hover:text-black 
+                        bg-[#B5B7B8]/80 text-black hover:text-black 
                         duration-500 font-normal transition rounded-3xl 
-                        py-1 md:py-2 px-3 md:px-4 
-                        text-[10px] md:text-xl 
-                        hover:bg-[#626263]"
+                        py-2 md:py-3 px-6 md:px-8
+                        text-sm md:text-xl 
+                        hover:bg-[#626263]/90 backdrop-blur-sm"
                     >
                         Show more
                     </Btn>
                     
                     <Btn className="
-                        bg-[#191919] text-white 
+                        bg-black/70 text-white 
                         duration-500 transition 
                         font-normal rounded-3xl 
-                        py-1 md:py-2 px-2 md:px-4 
-                        text-[10px] md:text-xl 
-                        hover:bg-[#626263] hover:text-white"
+                        py-2 md:py-3 px-6 md:px-8 
+                        text-sm md:text-xl 
+                        hover:bg-[#626263] hover:text-white backdrop-blur-sm"
                     >
                         Configurator
                     </Btn>
                 </div>
             </div>
-
         </div>
-    )
+    );
 };
 
 export default PhotoAudi;

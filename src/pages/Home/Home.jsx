b83e11f62090e59cb7b7b2a6b164dc852d50fdb6
andriydrob <drobandriy2000@gmail.com>
2025-11-25 21:33:24 +0200

    Added Btn and text on video

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import PhotoAudi from '../../components/UI/PhotoAudi';
 import PhotoCard from '../../components/UI/PhotoCard';
 import SpecItem from '../../components/UI/SpecItem';
 import VideoAudi from '../../components/UI/VideoAudi';
+import Btn from '../../components/UI/Btn';
 
 const Audi_Rs3_photo = "https://res.cloudinary.com/dh4jjre2h/image/upload/v1763856049/Audi_Rs3_photo_pml9ns.png";
 
@@ -61,8 +62,17 @@ const titles5 = ["Audi A7 Sportback", "Audi S7 Performance"];
 
     return(
         <div>
-            <BgVideo />
-            <Header />
+            <div className='relative'>
+                <BgVideo />
+                <Header />
+                <div className='relative'>
+                    <p className="absolute md:bottom-35 md:left-15 bottom-22 left-10 Futura New text-lg sm:text-2xl md:text-3xl text-white inline-block">EXPERIENCE PEAK PERFORMANCE.</p>
+                    <Btn className="bg-[#B5B7B8]/80 left-1/2 md:rounded-4xl -translate-x-1/2 w-[90%] md:w-auto md:left-47 md:bottom-15 bottom-10 absolute z-10 text-black hover:text-black duration-500 font-normal transition rounded-[20px] py-2 md:py-3 px-6 md:px-8 text-sm md:text-xl hover:bg-[#626263]/90 backdrop-blur-sm">
+                        AUDI SPORT MODELS
+                    </Btn>
+                </div>
+
+            </div>
             <section className='bg-[#191919] py-8 md:py-8 flex justify-around items-start border-t border-b border-gray-800'>
                 <SpecItem 
                 label="Power"

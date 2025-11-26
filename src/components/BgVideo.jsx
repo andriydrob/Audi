9e44ce834c0d7e013ede02b51ae30f8e9303e7ae
andriydrob <drobandriy2000@gmail.com>
2025-11-26 22:46:19 +0200

    Added video pause for optimization

--- src/components/BgVideo.jsx
+++ src/components/BgVideo.jsx
@@ -1,15 +1,51 @@
+import React, { useRef, useEffect } from 'react';
+
 const BackgroundVideoUrl = "https://res.cloudinary.com/dh4jjre2h/video/upload/v1763855891/background21_erab8v.mp4";
 
 const BgVideo = () => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const videoElement = videoRef.current;
+
+        if (!videoElement) return;
+        videoElement.loop = true;
+        
+        const handleIntersection = (entries) => {
+            entries.forEach(entry => {
+                if (entry.intersectionRatio < 0.5) {
+                    videoElement.pause();
+                } else {
+                    videoElement.play().catch(error => {
+                        console.error("Помилка відтворення фонового відео:", error);
+                    });
+                }
+            });
+        };
+
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: [0.5],
+        };
+
+        const observer = new IntersectionObserver(handleIntersection, options);
+
+        observer.observe(videoElement);
+        
+        return () => {
+            observer.unobserve(videoElement);
+        };
+    }, []);
+
     return (
-        <div className="top-0 left-0 w-screen
-         h-screen overflow-hidden -z-50 relative
-          md:w-screen md:h-screen md:relative">
+        <div className="top-0 left-0 w-screen h-screen 
+                        overflow-hidden -z-50 relative 
+                        md:w-screen md:h-screen md:relative">
             <video
-                autoPlay
+                ref={videoRef}
                 muted
                 playsInline
-                loop
                 id="myVideo"
                 src={BackgroundVideoUrl}
                 type="video/mp4"
@@ -18,7 +54,7 @@ const BgVideo = () => {
             >
             </video>
         </div>
-    )
+    );
 }
 
 export default BgVideo;
